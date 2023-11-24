ccd939dba8a118c55b76768bddc37bdc20433adb
Denis Kosgei <dennis1kosgei@gmail.com>
2023-11-24 10:44:05 -0800

    file changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,6 @@
 # Puppet Manifest: Create a file in /tmp/school
-# File path: /tmp/school
-# File permission: 0744
-# File owner: www-data
-# File group: www-data
-# File content: I love Puppet
-
 file { '/tmp/school':
   ensure   => file,
-  path     => '/tmp/school'
   mode     => '0744',
   owner    => 'www-data',
   group    => 'www-data',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
 # This Puppet manifest installs Flask version 2.1.0 from pip3
-class flask_installation {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  exec { 'install flask':
-    command => 'pip3 install Flask==2.1.0',
-    path    => ['/usr/bin'],
-    unless  => 'pip3 show Flask | grep -q "Version: 2.1.0"',
-  }
+# Installs 'flask' from 'pip3' using puppet
+package{'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-include flask_installation
